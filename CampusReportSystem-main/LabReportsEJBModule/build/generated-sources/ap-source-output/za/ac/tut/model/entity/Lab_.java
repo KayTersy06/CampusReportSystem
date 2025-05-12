e4ecb296fb297fa7527b1e1d75d2c516fb78f6f9
Clifford Maswanganye <cliffordswangi27@gmail.com>
2025-05-12 10:11:05 -0700

    halaa

---
+++ CampusReportSystem-main/LabReportsEJBModule/build/generated-sources/ap-source-output/za/ac/tut/model/entity/Lab_.java
@@ -0,0 +1,18 @@
+package za.ac.tut.model.entity;
+
+import java.util.Date;
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+import za.ac.tut.model.entity.Report;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2025-05-12T08:15:39")
+@StaticMetamodel(Lab.class)
+public class Lab_ { 
+
+    public static volatile SingularAttribute<Lab, Date> date;
+    public static volatile SingularAttribute<Lab, Long> id;
+    public static volatile SingularAttribute<Lab, Report> rep;
+    public static volatile SingularAttribute<Lab, String> Name;
+
+}
